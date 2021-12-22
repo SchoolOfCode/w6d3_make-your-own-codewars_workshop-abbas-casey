f513ad460107ccf52ff98fb6cf32a3f42bcb18b1
casey <cassandra.d.mclellan@gmail.com>
2021-12-22 15:48:43 +0000

    test for only numbers in array

--- main.js
+++ main.js
@@ -8,6 +8,8 @@
     Input is an array of numbers and strings.
     Function should return an array with two nested two arrays: one of only the numbers, and one of only the strings.
     ['a', 1, 'b', 2] => [['a', 'b'], [1,2]]
+
+    If there is any string that isn't a letter then throw this error: "this aint it cheif"
 */
 
 export function separateNumbersAndStrings(arr) {
--- main.test.js
+++ main.test.js
@@ -3,12 +3,23 @@
 import { separateNumbersAndStrings } from "./main.js";
 
 describe("separateNumbersAndStrings", () => {
-    it("should return an array of numbers and an array of strings, nested into one array.", () => {
+    it("should return an array of strings and an array of numbers, nested into one array.", () => {
         //['a', 1, 'b', 2] => [['a', 'b'], [1,2]]
         const input = ["a", 1, "b", 2];
         const expected = [
             ["a", "b"],
             [1, 2],
+        ];
+        const actual = separateNumbersAndStrings(input);
+
+        expect(actual).toStrictEqual(expected);
+    });
+
+    it("for an input of array containing only numbers, should return an array nested with an empty array and the array of numbers", () => {
+        const input = [1, 2, 3, 4, 1131, 12313, 921987];
+        const expected = [
+            [],
+            [1, 2, 3, 4, 1131, 12313, 921987],
         ];
         const actual = separateNumbersAndStrings(input);
 
