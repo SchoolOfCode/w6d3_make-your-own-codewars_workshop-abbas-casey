da0cabd82e230b4324e94cec42ba36e2589e9394
casey <cassandra.d.mclellan@gmail.com>
2021-12-22 16:14:31 +0000

    solved the function problem

--- main.js
+++ main.js
@@ -9,9 +9,20 @@
     Function should return an array with two nested two arrays: one of only the numbers, and one of only the strings.
     ['a', 1, 'b', 2] => [['a', 'b'], [1,2]]
 
-    If there is any string that isn't a letter then throw this error: "this aint it cheif"
+    If there is any string that isn't a letter or a number then throw this error: "this aint it cheif"
 */
 
 export function separateNumbersAndStrings(arr) {
-    // code here
+    let stringArr = arr.filter((arr) => {
+        let regex = /[a-zA-Z0-9]/g
+        if (typeof arr === "string") {
+            if (!arr.match(regex)) {
+                throw new Error("this aint it cheif");
+            }
+        }
+        return typeof arr === "string"
+    });
+    let numberArr = arr.filter((arr) => typeof arr === "number");
+    let bothArr = [stringArr, numberArr];
+    return bothArr;
 }
