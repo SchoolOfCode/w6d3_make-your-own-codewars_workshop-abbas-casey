cbcf7c5890b148dfc38f58db42a5626fd1c58c5f
EzyhB <ezyhbmail@gmail.com>
2021-12-22 15:41:28 +0000

    2nd test

--- main.test.js
+++ main.test.js
@@ -5,10 +5,22 @@ import { separateNumbersAndStrings } from "./main.js";
 describe("separateNumbersAndStrings", () => {
     it("should return an array of numbers and an array of strings, nested into one array.", () => {
         //['a', 1, 'b', 2] => [['a', 'b'], [1,2]]
-        const input = ['a', 1, 'b', 2];
-        const expected = [['a', 'b'], [1,2]];
+        const input = ["a", 1, "b", 2];
+        const expected = [
+            ["a", "b"],
+            [1, 2],
+        ];
         const actual = separateNumbersAndStrings(input);
 
         expect(actual).toStrictEqual(expected);
     });
-})
+});
+
+describe("is the things input right???", () => {
+    it("should only contain letters and numbers", () => {
+        const input = [".", 1, "-", 2];
+        const actual = () => separateNumbersAndStrings(input);
+
+        expect(actual).toThrow("this aint it cheif");
+    });
+});
