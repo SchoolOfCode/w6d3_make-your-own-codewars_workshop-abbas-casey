3e547820bb871d31c2fb581fc0f097a88341c0f9
casey <cassandra.d.mclellan@gmail.com>
2021-12-22 15:34:27 +0000

    made one test

--- main.js
+++ main.js
@@ -3,3 +3,13 @@
 */
 
 //👉 Write the function your CodeWarriors will start with below here:
+
+/*
+    Input is an array of numbers and strings.
+    Function should return an array with two nested two arrays: one of only the numbers, and one of only the strings.
+    ['a', 1, 'b', 2] => [['a', 'b'], [1,2]]
+*/
+
+export function separateNumbersAndStrings(arr) {
+    // code here
+}
--- main.test.js
+++ main.test.js
@@ -1,1 +1,14 @@
 //👉 Write your tests below here:
+
+import { separateNumbersAndStrings } from "./main.js";
+
+describe("separateNumbersAndStrings", () => {
+    it("should return an array of numbers and an array of strings, nested into one array.", () => {
+        //['a', 1, 'b', 2] => [['a', 'b'], [1,2]]
+        const input = ['a', 1, 'b', 2];
+        const expected = [['a', 'b'], [1,2]];
+        const actual = separateNumbersAndStrings(input);
+
+        expect(actual).toStrictEqual(expected);
+    });
+})
