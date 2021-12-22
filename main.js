efeff35b1452f2cd165727fe9732b0995629d14f
casey <cassandra.d.mclellan@gmail.com>
2021-12-22 16:50:33 +0000

    deleted solution, to prepare for sharing the kata

--- main.js
+++ main.js
@@ -1,28 +1,11 @@
-/* 
-👉 Write your kata here!
-*/
-
-//👉 Write the function your CodeWarriors will start with below here:
-
 /*
     Input is an array of numbers and strings.
-    Function should return an array with two nested two arrays: one of only the numbers, and one of only the strings.
+    The function should return an array with two nested arrays: one of only the numbers, and one of only the strings.
     ['a', 1, 'b', 2] => [['a', 'b'], [1,2]]
 
     If there is any string that isn't completely letters or numbers (including any whitespace!) then throw this error: "this aint it chief"
 */
 
 export function separateNumbersAndStrings(arr) {
-    let stringArr = arr.filter((arr) => {
-        let regex = /\W/g
-        if (typeof arr === "string") {
-            if (arr.match(regex)) {
-                throw new Error("this aint it chief");
-            }
-        }
-        return typeof arr === "string"
-    });
-    let numberArr = arr.filter((arr) => typeof arr === "number");
-    let bothArr = [stringArr, numberArr];
-    return bothArr;
+    // Your solution here! :)
 }
